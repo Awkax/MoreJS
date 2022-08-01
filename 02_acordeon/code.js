a6f706e95ba567049eef7ef2d4bde63fc14f1744
Awka <47970564+Awkax@users.noreply.github.com>
2022-08-01 12:38:51 +0200

    Accordion

---
+++ 02_acordeon/code.js
@@ -0,0 +1,44 @@
+function a() {
+/*JS del acordeon*/
+	var acc = document.getElementsByClassName("accordion");
+	var i;
+
+	for (i = 0; i < acc.length; i++) {
+		acc[i].addEventListener("click", function () {
+			this.classList.toggle("active");
+			var panel = this.nextElementSibling;
+			if (panel.style.maxHeight) {
+				panel.style.maxHeight = null;
+			} else {
+				panel.style.maxHeight = panel.scrollHeight + "px";
+			}
+		});
+	}
+/*JS del slider (no he conseguido hacer que se pudiera ir de una imagen a otra aprentando los puntitos de abajo correspondientes pero tampoco era una prioridad asi que no me he empeñado mas en hacerlo)*/
+	var slideIndex = 0;
+	showSlides();
+
+	function currentSlide(n) {
+		showSlides(slideIndex = n);
+	}
+
+	function showSlides() {
+		var i;
+		var slides = document.getElementsByClassName("mySlides");
+		var dots = document.getElementsByClassName("dot");
+		for (i = 0; i < slides.length; i++) {
+			slides[i].style.display = "none";
+		}
+		slideIndex++;
+		if (slideIndex > slides.length) {
+			slideIndex = 1;
+		}
+		for (i = 0; i < dots.length; i++) {
+			dots[i].className = dots[i].className.replace(" active", "");
+		}
+		slides[slideIndex - 1].style.display = "block";
+		dots[slideIndex - 1].className += " active";
+		setTimeout(showSlides, 2000); // Change image every 2 seconds
+	}
+
+}
